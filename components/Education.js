fa25b3174cd6d1631acd6ad748b59bac6d9ffc19
randyglasgow <glasgowrandy@gmail.com>
2021-08-14 13:04:52 -0400

    Updates padding on education

--- components/Education.js
+++ components/Education.js
@@ -3,7 +3,7 @@ export default function Education({ school, degree, concentration, end }) {
     <div className="my-8">
       <div className="flex flex-col md:flex-row justify-between">
         <h3 className="text-2xl text-center md:text-left">{school}</h3>
-        <div className='text-center md:text-left'>{end}</div>
+        <div className='text-center md:text-left text-lg font-thin'>{end}</div>
       </div>
       <div className='text-center md:text-left'>
         {degree} in {concentration}
