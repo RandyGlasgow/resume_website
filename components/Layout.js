ca48c4a0ee03148e42c32ffb2dacf11b2592985b
randyglasgow <glasgowrandy@gmail.com>
2021-08-14 12:31:05 -0400

    Updates padding

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,11 @@ import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
-    <div className="">
+    <div className="py-24 bg-gradient-to-br from-gray-200 to-gray-400 min-h-screen">
       <Head></Head>
-      <body >
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 xl:px-36">
-          {children}
-        </div>
-      </body>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 xl:px-36">
+        {children}
+      </div>
     </div>
   );
 }
