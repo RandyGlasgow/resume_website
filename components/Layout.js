a5beb33624060e8df2760804b5972f6376bfc221
randyglasgow <glasgowrandy@gmail.com>
2021-08-14 12:32:35 -0400

    Removes padding

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
-    <div className="py-24 bg-gradient-to-br from-gray-200 to-gray-400 min-h-screen">
+    <div className="">
       <Head></Head>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 xl:px-36">
         {children}
