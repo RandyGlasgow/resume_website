b405dfbb8dd08a28644943aa8b6f62d237372e1f
randyglasgow <glasgowrandy@gmail.com>
2021-08-14 12:43:42 -0400

    Updates mobile padding

--- components/Company.js
+++ components/Company.js
@@ -2,7 +2,7 @@ export default function Company({ employer, start, end, role, children }) {
   return (
     <div className="my-8">
       <div className="flex flex-col md:flex-row justify-between items-center">
-        <h3 className="text-3xl">{employer}</h3>
+        <h3 className="text-3xl text-center md:text-left">{employer}</h3>
         <div className="text-lg font-thin">
           {start} to {end}
         </div>
