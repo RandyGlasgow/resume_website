aa10182bf7217a2aba3efe091157fb8e97c71747
randyglasgow <glasgowrandy@gmail.com>
2021-08-14 17:37:15 -0400

    Adds lang attribute to html tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html className="py-24 bg-gradient-to-br from-gray-200 to-gray-400 min-h-screen">
+      <Html
+        className="py-24 bg-gradient-to-br from-gray-200 to-gray-400 min-h-screen"
+        lang="en-us"
+      >
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
